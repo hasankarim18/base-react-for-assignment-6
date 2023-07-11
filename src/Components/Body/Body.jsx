f6232b4fd34a8f5fd8f259120054edb795f8b9cc
hasankarim18 <hallysust@gmail.com>
2023-07-11 20:53:19 +0600

    toggle text and fetch new quote at every 10 seconds

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,11 +1,48 @@
+import { useState } from "react";
 
 
 const Body = () => {
+
+    const [isShown, setIsShown] = useState(false)
+    const text =
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos repellat reprehenderit animi iste incidunt odit tenetur asperiores officia temporibus doloribus, tempora cumque, blanditiis eaque et expedita numquam? Veniam, impedit eveniet.";
+
+    const handleShowText = ()=> {
+        setIsShown((prev)=> !prev)
+    }
+
     return (
-        <div>
-            Body
+      <div>
+        <div className="h-full min-h-[calc(100vh-10rem)] flex justify-center items-center gap-8 flex-col ">
+          <button
+            onClick={handleShowText}
+            className={`btn ${
+              isShown ? "bg-green-400" : "bg-red-400"
+            } py-2 px-4 rounded-xl`}
+          >
+            {isShown ? "Show Text" : "Hide Text"}
+          </button>
+          <div className="flex  flex-col items-center justify-center text-bold text-xl ">
+            Text:{" "}
+            <p
+              className={`text-center w-full md:w-1/2  p-2 ${
+                !isShown && "rounded-xl border"
+              } `}
+            >
+              {(() => {
+                if (!isShown) {
+                  return text;
+                } else {
+                  return "";
+                }
+              })()}
+            </p>
+          </div>
         </div>
+      </div>
     );
 };
 
 export default Body;
+
+// @Hasan18
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,101 @@
+import { useEffect, useState } from "react";
 
+    
+
+  const getQuote = (url, api_key) => {
+    return fetch(url, {
+      headers: { "X-Api-Key": api_key },
+      contentType: "application/json",
+    });
+  };
 
 const Footer = () => {
+    const [count, setCount] = useState(10)
+    const [setsetLoading, setSetsetLoading] = useState(false)
+     const api_key = import.meta.env.VITE_api_key; 
+     const [quote, setQuote] = useState('')
+    // console.log(api_key);
+
+     const url = `https://api.api-ninjas.com/v1/quotes?category=happiness`;  
+
+     useEffect(() => {
+        setSetsetLoading(true)
+        getQuote(url, api_key)
+         .then((res) => res.json())
+         .then((data) => {
+            setSetsetLoading(false)
+            setQuote(data[0].quote)        
+         })
+         .catch((err) => {
+           console.log(err);
+           setSetsetLoading(false);
+         });          
+     }, [api_key, url ])
+
+     useEffect(() => {
+       const timer = setTimeout(() => {
+        if(count > 0){
+         setCount((prev) => prev - 1);
+        }
+       }, 1000);
+     
+       return () => {
+            clearTimeout(timer) 
+       }
+     }, [count])
+
+    useEffect(() => {
+        if(count === 0){
+            setSetsetLoading(true)
+            getQuote(url, api_key)
+            .then(res => res.json())
+            .then(data => {
+                setQuote(data[0].quote);      
+                setSetsetLoading(false)  
+                setCount(10)
+            })
+            .catch(err => {
+                console.log(err);
+                setSetsetLoading(false);
+            })
+        }
+    }, [count, url, api_key])
+    
+
+
+    // console.log(count);
+     
+     
+
     return (
-        <div className="h-20 bg-black text-white">
-           Footer
-            
+      <div className="min-h-20 bg-black text-white">
+        <div className="p-4">
+          <div className="flex justify-between text-xl">
+            <div>Fetch New Quote Every 10 second</div>
+            <div>{ !setsetLoading ? count: "Loading..."}</div>
+          </div>
+          <div className="bg-white text-black p-2 mt-2 font-bold" >
+           <span className="text-bold text-red-400">Quote</span> {quote}
+          </div>
         </div>
+      </div>
     );
 };
 
 export default Footer;
+
+
+/*
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+    headers: { 'X-Api-Key': 'YOUR_API_KEY'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
+*/
